e4fb819eb70a62b1a8b744e5be4ff22ebb133f27
dariour <dario.ursulin@gmail.com>
2018-03-20 10:47:07 +0100

    Update options.js

--- scripts/options.js
+++ scripts/options.js
@@ -1,22 +1,22 @@
 // Saves options to chrome.storage
 function saveOptions() {
-   let consumer_key = document.getElementById('consumer-key').value;
-   let consumer_secret = document.getElementById('consumer-secret').value;
-   let site_url = document.getElementById('site-url').value;
-   let options = {
-        "consumerKey": consumer_key,
-        'consumerSecret': consumer_secret,
-        'siteURL': site_url
-    };
+	let consumer_key = document.getElementById('consumer-key').value;
+	let consumer_secret = document.getElementById('consumer-secret').value;
+	let site_url = document.getElementById('site-url').value;
+	let options = {
+		"consumerKey": consumer_key,
+		'consumerSecret': consumer_secret,
+		'siteURL': site_url
+	};
 	chrome.storage.sync.set({
 		'options': options
 	}, function() {
-    let status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
+		let status = document.getElementById('status');
+		status.textContent = 'Options saved.';
+		setTimeout(function() {
 		status.textContent = '';
-    }, 750);
-  });
+		}, 750);
+	});
 }
 
 // Restores options frome chrome.storage
@@ -27,7 +27,8 @@ function restoreOptions() {
 	document.getElementById('consumer-key').value = items.options.consumerKey;
 	document.getElementById('consumer-secret').value = items.options.consumerSecret;
 	document.getElementById('site-url').value = items.options.siteURL;
-	});
+		}
+	);
 }
 document.addEventListener('DOMContentLoaded', restoreOptions);
 document.getElementById('save').addEventListener('click', saveOptions);
